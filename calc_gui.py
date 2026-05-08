58da503f20cea08b35d693efb23224c9e8b9e3c7
JRodZemu <chronozemu@proton.me>
2026-05-09 01:36:39 +0200

    I tried to put a feature to open the application with no toolbar and trying to calculate the middle of the screen to center it

--- calc_gui.py
+++ calc_gui.py
@@ -2,7 +2,19 @@ from tkinter import *
 
 root = Tk()
 root.title("Calculadora de prueba")
+root.overrideredirect(True)
 root.resizable(0,0)
+
+root.update_idletasks()
+
+# Método para centrarlo en medio. No me gusta, ya que en doble monitor podría fallar
+# width = 500
+# height = 500
+# width_screen = root.winfo_screenwidth()
+# height_screen = root.winfo_screenheight()
+# x = (width_screen // 2) - (width // 2)
+# y = (height_screen // 2) - (height // 2)
+# root.geometry(f"{width}x{height}+{x}+{y}")
 
 
 
